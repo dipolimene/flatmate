24f86fbad59c37e5736a1f77f5dfd1b3724e12ee
dipolimene <dipotope@yahoo.com>
2018-01-17 13:53:16 +0100

    created a view controller for the index page

---
+++ app/Http/Controllers/ViewController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ViewController extends Controller
+{
+    //to view the index page
+    public function show(){
+    	return view('view.index');
+    }
+}
