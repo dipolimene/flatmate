a57831c93226b2a0af25fe5d5133b6a5f0a4c1f8
dipolimene <dipotope@yahoo.com>
2018-01-17 14:27:14 +0100

    change the sign up and sin in icons

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -13,7 +13,7 @@
 	                    <img src="assets/img/logo.png" alt="Creative Tim Logo" rel="tooltip" title="<b>Material Kit</b> was Designed & Coded with care by the staff from <b>Creative Tim</b>" data-placement="bottom" data-html="true">
 	                </div>
 	                <div class="brand">
-	                    Creative Tim
+	                    Flat Mate
 	                </div>
 
 
@@ -25,26 +25,26 @@
 	    	<ul class="nav navbar-nav navbar-right">
 				<li>
 					<a href="components-documentation.html" target="_blank">
-						<i class="material-icons">dashboard</i> Components
+						<i class="material-icons">face</i> Sign Up
 					</a>
 				</li>
 				<li>
 					<a href="http://demos.creative-tim.com/material-kit-pro/presentation.html?ref=utp-freebie" target="_blank">
-						<i class="material-icons">unarchive</i> Upgrade to PRO
+						<i class="material-icons">fingerprint</i> Sign In
 					</a>
 				</li>
 				<li>
-					<a rel="tooltip" title="Follow us on Twitter" data-placement="bottom" href="https://twitter.com/CreativeTim" target="_blank" class="btn btn-white btn-simple btn-just-icon">
+					<a rel="tooltip" title="Follow us on Twitter" data-placement="bottom" href="/" target="_blank" class="btn btn-white btn-simple btn-just-icon">
 						<i class="fa fa-twitter"></i>
 					</a>
 				</li>
 				<li>
-					<a rel="tooltip" title="Like us on Facebook" data-placement="bottom" href="https://www.facebook.com/CreativeTim" target="_blank" class="btn btn-white btn-simple btn-just-icon">
+					<a rel="tooltip" title="Like us on Facebook" data-placement="bottom" href="/" target="_blank" class="btn btn-white btn-simple btn-just-icon">
 						<i class="fa fa-facebook-square"></i>
 					</a>
 				</li>
 				<li>
-					<a rel="tooltip" title="Follow us on Instagram" data-placement="bottom" href="https://www.instagram.com/CreativeTimOfficial" target="_blank" class="btn btn-white btn-simple btn-just-icon">
+					<a rel="tooltip" title="Follow us on Instagram" data-placement="bottom" href="/" target="_blank" class="btn btn-white btn-simple btn-just-icon">
 						<i class="fa fa-instagram"></i>
 					</a>
 				</li>
